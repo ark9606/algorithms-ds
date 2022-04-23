2eb2af66d343ae1073ea5428f6b30e83af4d9d3c
Arkady Zelensky <arkady.zelensky@yalantis.com>
2022-04-23 23:05:54 +0300

    added extractMax

--- src/data-structures/07_max_binary_heap.ts
+++ src/data-structures/07_max_binary_heap.ts
@@ -1,27 +1,15 @@
 class MaxBinaryHeap {
-  values;
+  values: number[];
   constructor() {
     this.values = [];
   }
 
-  insert0(value) {
-    this.values.push(value);
-    let index = this.values.length - 1;
-    let element = this.values[index];
-    let parentIndex = Math.floor((index - 1) / 2);
-
-    while(element > this.values[parentIndex]) {
-      let parent = this.values[parentIndex];
-      this.values[parentIndex] = element;
-      this.values[index] = parent;
-
-      index = parentIndex;
-      parentIndex = Math.floor((index - 1) / 2);
-    }
-  }
-
   insert(value) {
     this.values.push(value);
+    this.bubbleUp();
+  }
+
+  private bubbleUp() {
     let index = this.values.length - 1;
     let element = this.values[index];
 
@@ -32,6 +20,67 @@ class MaxBinaryHeap {
       this.values[index] = parent;
 
       index = parentIndex;
+    }
+  }
+
+  // like remove
+  extractMax() {
+    if (this.values.length === 0) {
+      return null;
+    }
+    const max = this.values[0];
+    const last = this.values.pop();
+    if (this.values.length === 0) {
+      return max;
+    }
+    this.values[0] = last;
+    this.siftDown();
+    return max;
+  }
+
+  private siftDown() {
+    let parentInd = 0;
+    const length = this.values.length;
+    while (true) {
+      const leftChildInd = 2 * parentInd + 1;
+      const rightChildInd = 2 * parentInd + 2;
+      const leftBiggerThenParent = leftChildInd < length && this.values[parentInd] < this.values[leftChildInd];
+      const rightBiggerThenParent = rightChildInd < length && this.values[parentInd] < this.values[rightChildInd];
+      if (leftBiggerThenParent && rightBiggerThenParent) {
+        const leftTheBiggest = this.values[leftChildInd] > this.values[rightChildInd];
+        if (leftTheBiggest) {
+          // swap with left
+          let temp = this.values[parentInd];
+          this.values[parentInd] = this.values[leftChildInd];
+          this.values[leftChildInd] = temp;
+          parentInd = leftChildInd;
+        }
+        else {
+          // swap with right
+          let temp = this.values[parentInd];
+          this.values[parentInd] = this.values[rightChildInd];
+          this.values[rightChildInd] = temp;
+          parentInd = rightChildInd;
+        }
+      }
+      else if (leftBiggerThenParent && !rightBiggerThenParent) {
+        // swap with left
+        let temp = this.values[parentInd];
+        this.values[parentInd] = this.values[leftChildInd];
+        this.values[leftChildInd] = temp;
+        parentInd = leftChildInd;
+      }
+      else if (rightBiggerThenParent && !leftBiggerThenParent) {
+        // swap with right
+        let temp = this.values[parentInd];
+        this.values[parentInd] = this.values[rightChildInd];
+        this.values[rightChildInd] = temp;
+        parentInd = rightChildInd;
+      }
+      else if (!leftBiggerThenParent && !rightBiggerThenParent) {
+        // leave parent on its place
+        break;
+      }
     }
   }
 
@@ -50,5 +99,25 @@ heap.insert(15);
 heap.insert(12);
 heap.insert(18);
 heap.insert(20);
-console.log('heap: ' + heap);
+console.log('Original heap: ' + heap);
 
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
+console.log('\nExtracted max: ' + heap.extractMax());
+console.log('Heap: ' + heap);
